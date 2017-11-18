c897947ad182dd14849ddae3d1e22890e3a56ba6
Adam <davidatomhernandez@gmail.com>
2017-11-18 17:15:28 -0500

    Update markup; server for two images

--- server.go
+++ server.go
@@ -1,60 +1,49 @@
 package main
 
 import (
-	"bytes"
+	"crypto/md5"
 	"fmt"
+	"html/template"
 	"io"
-	"io/ioutil"
-	"mime/multipart"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
-func postFile(filename string, targetUrl string) error {
-	bodyBuf := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuf)
+// upload logic
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
 
-	// this step is very important
-	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-	if err != nil {
-		fmt.Println("error writing to buffer")
-		return err
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile1")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/image1", os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
-
-	// open file handle
-	fh, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("error opening file")
-		return err
-	}
-	defer fh.Close()
-
-	//iocopy
-	_, err = io.Copy(fileWriter, fh)
-	if err != nil {
-		return err
-	}
-
-	contentType := bodyWriter.FormDataContentType()
-	bodyWriter.Close()
-
-	resp, err := http.Post(targetUrl, contentType, bodyBuf)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	resp_body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(resp.Status)
-	fmt.Println(string(resp_body))
-	return nil
 }
 
-// sample usage
 func main() {
-	target_url := "http://localhost:9090/upload"
-	filename := "./astaxie.pdf"
-	postFile(filename, target_url)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/upload", upload)
+	log.Fatal(http.ListenAndServe(":7777", nil))
 }
