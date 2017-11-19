b28a9bc77017a6a82bce8457bbb6ea6bad9f4a2f
Adam <davidatomhernandez@gmail.com>
2017-11-19 18:16:15 -0500

    Save files to disc

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ let files = Object.seal([null, null]);
 // input.style.opacity = 0;
 inputOne.addEventListener('change', updateImageDisplay.bind(null, inputOne, previewOne));
 inputTwo.addEventListener('change', updateImageDisplay.bind(null, inputTwo, previewTwo));
+uploadedFiles = {};
 
 function updateImageDisplay(input, preview) {
   while(preview.firstChild) {
     preview.removeChild(preview.firstChild);
   }
+  const fileName = preview.className;
   var curFiles = input.files;
   if(curFiles.length === 0) {
     preview.textContent = 'No files currently selected for upload';
   } else {
     const file = curFiles[0];
+
+    uploadedFiles[fileName] = file;
+
     if(validFileType(file)) {
       const url = window.URL.createObjectURL(file);
       preview.src = url;
@@ -35,3 +40,37 @@ const fileTypes = [
 function validFileType(file) {
   return fileTypes.includes(file.type);
 }
+
+function uploadFiles() {
+  const fileCount = Object.keys(uploadedFiles).length;
+  if (fileCount !== 2) {
+    window.alert('please include a total of two faces');
+    return;
+  }
+  const uri = '/upload';
+  const formData = new FormData();
+  const files = Object.values(uploadedFiles);
+  console.log(files);
+  files.forEach((file, index)=> {
+    formData.append(`file-${index}`, file);
+  });
+  for (var par of formData.entries()) {
+    console.log(par[0] + ',' + par[1]);
+  }
+
+  fetch(uri, {
+    method: 'POST',
+    'Content-Type': 'multipart/form-data',
+    body: formData
+  }).then(res => {
+    console.log(res);
+  }).catch(e => {
+    console.log('Error', e);
+  })
+}
+
+const form = document.getElementById('upload-form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  uploadFiles();
+})
--- server.go
+++ server.go
@@ -1,45 +1,44 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 )
 
-// upload logic
-func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
-	} else {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("uploadfile1")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./test/image1", os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer f.Close()
-		io.Copy(f, file)
+func saveFile(fileName string, r *http.Request) {
+	file, _, err := r.FormFile(fileName)
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer file.Close()
+	filepath := fmt.Sprintf("./test/%v", fileName)
+
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Println("GET used, implement error here!")
+	} else {
+		fmt.Println("Receiving Files...")
+	}
+	r.ParseMultipartForm(32 << 20)
+	files := r.MultipartForm.File
+
+	fmt.Printf("Saving %v files...\n", len(files))
+	for fileName := range files {
+		saveFile(fileName, r)
+	}
+	fmt.Print("files saved.")
 }
 
 func main() {
