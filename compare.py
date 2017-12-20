a3a99e46c37482a0b9731d82bdd0bacb94c9761f
Adam <davidatomhernandez@gmail.com>
2017-12-19 22:29:23 -0500

    Return error codes for exceptions

--- compare.py
+++ compare.py
@@ -24,19 +24,28 @@ align = openface.AlignDlib(os.path.join(dlibModelDir, "shape_predictor_68_face_l
 img_dim = 96
 net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), img_dim)
 
+class ImageLoadError(Exception):
+    pass
+
+class NoFaceError(Exception):
+    pass
+
+class CouldNotAlignFacesError(Exception):
+    pass
+
 def get_rep(b64_string):
     bgrImg = b64_to_cv2_img(b64_string)
     if bgrImg is None:
-        raise Exception("Unable to load image: {}".format(imgPath))
+        raise ImageLoadError 
     rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
 
     bb = align.getLargestFaceBoundingBox(rgbImg)
     if bb is None:
-        raise Exception("Unable to find a face: {}".format(imgPath))
+        raise NoFaceError 
     alignedFace = align.align(img_dim, rgbImg, bb,
                               landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
     if alignedFace is None:
-        raise Exception("Unable to align image: {}".format(imgPath))
+        raise CouldNotAlignFacesError 
     rep = net.forward(alignedFace)
     return rep
 
@@ -57,9 +66,16 @@ def main():
     img2 =  raw_input('reading image two from stdin!')
     img1 = ensure_padding(img1)
     img2 = ensure_padding(img2)
-    d = get_rep(img1) - get_rep(img2)
-    diff = np.dot(d, d)
-    print "L2 distance was calculated to be {}!".format(diff)
+    try: 
+        d = get_rep(img1) - get_rep(img2)
+        diff = np.dot(d, d)
+        print "L2 distance was calculated to be {}!".format(diff)
+    except ImageLoadError:
+        return 9001
+    except (NoFaceError, CouldNotAlignFacesError):
+        return 9001
+    except:
+        return 9003
     return ((4.0 - diff)/4.0) * 100
 
 if __name__ == "__main__":
